a8623e51311313677511e2f5a86de20ba08b7214
ttrader88 <ttrader88@mail.ru>
2024-02-05 16:36:34 +1200

    Updates MaxMoneyOut blockchain parameter

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -276,7 +276,7 @@ public:
         consensus.nFutureTimeDriftPoW = 7200;
         consensus.nFutureTimeDriftPoS = 180;
         consensus.nMasternodeCountDrift = 20;           // num of MN we allow the see-saw payments to be off by
-        consensus.nMaxMoneyOut = 100000000 * COIN;      // total num
+        consensus.nMaxMoneyOut = 10000000000 * COIN;      // total num
         consensus.nPoolMaxTransactions = 3;
         consensus.nProposalEstablishmentTime = 60 * 60 * 24;    // must be at least a day old to make it into a budget
         consensus.nStakeMinAge = 60 * 60; // 1h
@@ -477,7 +477,7 @@ public:
         consensus.nFutureTimeDriftPoW = 7200;
         consensus.nFutureTimeDriftPoS = 180;
         consensus.nMasternodeCountDrift = 4;        // num of MN we allow the see-saw payments to be off by
-        consensus.nMaxMoneyOut = 100000000 * COIN;  // total num
+        consensus.nMaxMoneyOut = 10000000000 * COIN;  // total num
         consensus.nPoolMaxTransactions = 2;
         consensus.nProposalEstablishmentTime = 60 * 5;  // at least 5 min old to make it into a budget
         consensus.nStakeMinAge = 60 * 60;
@@ -618,7 +618,7 @@ public:
         consensus.nFutureTimeDriftPoW = 7200;
         consensus.nFutureTimeDriftPoS = 180;
         consensus.nMasternodeCountDrift = 4;        // num of MN we allow the see-saw payments to be off by
-        consensus.nMaxMoneyOut = 100000000 * COIN;  // total num
+        consensus.nMaxMoneyOut = 10000000000 * COIN;  // total num
         consensus.nPoolMaxTransactions = 3;
         consensus.nProposalEstablishmentTime = 60 * 5;  // at least 5 min old to make it into a budget
         consensus.nStakeMinAge = 0;
